f29a45ae08c185268ddd5763eb998dec0fe93d1f
Jarl Nartey <jarlnartey@gmail.com>
2022-08-26 03:08:48 +0200

    Completed version 1

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-const dateFormatter = (date) => {
-
-}
-
 app.get("/api/:date?", (req, res) => {
     let date = req.params.date
-    const utc = new Date(date)
-    const unix = utc.getTime()
-    let timeStamps = { "unix": unix, "utc": utc.toUTCString() }
-    res.send(timeStamps)
+
+    //Check if date format, else it is unix format
+    if (date.includes("-")) {
+
+        // Check if invalid date
+        if (!new Date(date).getDay()) return res.send({ error: "Invalid Date" })
+
+        // Send out dates
+        const utc = new Date(date)
+        const unix = utc.getTime()
+        let timeStamps = { "unix": unix, "utc": utc.toUTCString() }
+        res.send(timeStamps)
+    } else {
+
+        // Check if invalid date
+        if (!new Date(parseInt(date)).getDay()) return res.send({ error: "Invalid Date" })
+
+        // Send out dates
+        const utc = new Date(parseInt(date))
+        let timeStamps = { "unix": parseInt(date), "utc": utc.toUTCString() }
+        res.send(timeStamps)
+    }
 })
-
-
 
 app.listen(port, () => {
     console.log("Im listening at: " + port)
