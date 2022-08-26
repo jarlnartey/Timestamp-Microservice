8f88d0d424ff8c525a416e16b424d3813dfce25f
Jarl Nartey <jarlnartey@gmail.com>
2022-08-26 02:03:09 +0200

    Add date to timestamp functionality

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const dateFormatter = (date) => {
 
-app.get("/", (req, res) => {
-    res.send("hello worldadfaf!")
+}
+
+app.get("/api/:date?", (req, res) => {
+    let date = req.params.date
+    const utc = new Date(date)
+    const unix = utc.getTime()
+    let timeStamps = { "unix": unix, "utc": utc.toUTCString() }
+    res.send(timeStamps)
 })
+
+
 
 app.listen(port, () => {
     console.log("Im listening at: " + port)
